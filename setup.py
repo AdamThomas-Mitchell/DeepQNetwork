caa486ae9c31882380e5f101cfeddd7707ba7994
AdamThomas-Mitchell <74470310+AdamThomas-Mitchell@users.noreply.github.com>
2023-06-09 17:07:53 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build original Deep Q-Network as proposed in thehe nNature paper by DeepMind: `Human-level control through deep reinforcement learning`.',
+    author='Adam Thomas',
+    license='',
+)
